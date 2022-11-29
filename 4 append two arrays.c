32f9fc7b65ecded38b746b1eed875264d8349673
ANULALSN <114857827+ANULALSN@users.noreply.github.com>
2022-11-29 11:32:13 +0530

    Add files via upload

---
+++ 4 append two arrays.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int a[10],b[10],c[20],a1,b1,i,j;
+	printf("Enter the number of elements of array1:\n");
+	scanf("%d",&a1);
+	printf("Enter the elements:\n");
+	for(i=0;i<a1;i++)
+		scanf("%d",&a[i]);
+	printf("Enter the number of elements of array2:\n");
+	scanf("%d",&b1);
+	printf("Enter the elements:\n");
+	for(i=0;i<b1;i++)
+		scanf("%d",&b[i]);
+	//merging
+	for(i=0;i<a1;i++)
+		c[i]=a[i];
+	for(j=0;j<b1;j++)
+		c[i+j]=b[j];
+	
+	printf("Arrays aftert merging\n");
+	for(i=0;i<a1+b1;i++)
+		printf("%d",c[i]);
+}
---
+++ 9 sort a singly linked list.c
@@ -0,0 +1,68 @@
+#include <stdio.h>  
+#include<stdlib.h>
+struct node{ 
+	int data;  
+	struct node *next;  
+};     
+struct node *head, *tail = NULL;  
+void addNode(int data) 
+{  
+	struct node *newNode = (struct node*)malloc(sizeof(struct node));  
+	newNode->data = data;  
+	newNode->next = NULL;  
+	if(head == NULL)
+	{  
+		head = newNode;  
+		tail = newNode;  
+	}  
+	else 
+	{  
+		tail->next = newNode;  
+		tail = newNode;  
+	}  
+}  
+
+void sortList()
+{ 
+	struct node *current = head, *index = NULL;  
+	int temp;
+	while(current != NULL) 
+	{   
+		index = current->next; 
+		while(index != NULL) 
+		{   
+			if(current->data > index->data) 
+			{  
+				temp = current->data;  
+				current->data = index->data;  
+				index->data = temp;  
+			}  
+			index = index->next;  
+		}  
+		current = current->next;  
+	}      
+} 
+void display()  
+{  
+	struct node *current = head;  
+	while(current != NULL) 
+	{  
+		printf("%d ", current->data);  
+		current = current->next;  
+	}  
+	printf("\n");  
+} 
+int main()  
+{  
+	addNode(9);  
+	addNode(7);  
+	addNode(2);  
+	addNode(5);  
+	addNode(4);  
+	printf("Original list: \n");  
+	display();
+	sortList();  
+	printf("Sorted list: \n");  
+	display();       
+	return 0;  
+}
